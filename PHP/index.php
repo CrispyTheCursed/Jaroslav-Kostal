cc6f32d280768addbf9c03f6fa207f1f14cdc26f
Jaroslav Košťál <62184655+CrispyTheCursed@users.noreply.github.com>
2020-04-04 21:35:08 +0200

    Domácí úkol - Příprava rozpracovaných dat

---
+++ PHP/index.php
@@ -0,0 +1,99 @@
+<?PHP echo 3*5;?>
+<br><br><br>
+    <?PHP echo date ('H:i:s')?><br>
+<br><br>
+    <?PHP echo "Ahoj"?><br><br>
+<?PHP 
+$Jmeno ='Jaroslav';
+echo $Jmeno; 
+$jmeno='Kostal';
+echo $jmeno;?>
+    <br><br><br>
+<?PHP 
+$cislo1 = 5;
+$cislo2 = 4;
+echo$cislo1 ?>
+    <br><br>
+    <?PHP
+    $a = 5 ;
+    $b = 4 ;
+    echo$a+$b; 
+    ?>
+    <br><br>
+<?PHP 
+$a = 5 ;
+$b = 4 ;
+echo $a-$b; ?>
+    <br><br>
+    <?PHP 
+    $a = 16;
+    $b = 4;
+    echo $a/$b; ?>
+    <br><br>
+    <?PHP 
+    $a = 6;
+    $b = 7;
+      echo $a*$b;?>
+    <br><br>
+  
+    <?PHP
+    $a = 5 ;
+    $a++;
+    echo$a; 
+    ?> 
+    <br><br>
+    <?PHP
+    $a = 5 ;
+    $a--;
+    echo$a; 
+    ?>  
+    <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 4;
+    echo $a+=$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 4;
+    echo $a-=$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 4;
+    echo $a*=$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 4;
+    echo $a/=$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 4;
+    echo $a==$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 1;
+    echo $a!=$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 7;
+    echo $a>$b; ?>
+ <br><br>
+     <?PHP 
+    $a = 16;
+    $b = 60;
+    echo $a<$b; ?>
+ <br><br>
+ <?PHP 
+ $a = 6;
+ $b = 1;
+ echo $a || $b; ?>
+ <br><br>
+<?PHP
+$a = 6;
+$b = 6;
+echo $a && $b; ?>
